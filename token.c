dcd2397bfd766d002fd04d159d3d34d6d19e97ff
Tomas Celaya <tjcelaya@gmail.com>
2013-09-25 18:46:42 -0400

    tokenize into array with count_tokens() and tokarr()

--- token.c
+++ token.c
@@ -2,47 +2,68 @@
 #include <stdlib.h>  
 #include <string.h>  
 #include <unistd.h>
-#include <pwd.h>  
-#define T 1
-#define F 0
 
-#define DEBUG T
+#define DEBUG 0
 
+/*  count_tokens
+ *      returns number of tokens in SPACE-DELIMITED string
+ *      accepts C-string
+ */
 
-char** tokenize(char* input, char** result) {
-    
+int count_tokens(char* input) {
+    const char* DELIM = " ";
     if (strlen(input) < 2)
         return NULL;
     
     char* input_copy = strdup(input);
-    const char* DELIM = " ";
-    char* tok = NULL;
-    char* rest = NULL;
-    char* p = input_copy;
-
+    
     int tokens = 1;
     char* next_tok = NULL;
     
     for (next_tok = strchr(input_copy, DELIM[0]);
             next_tok != NULL;
             next_tok = strchr(next_tok + 1, DELIM[0])) {
-        printf("%s\n", next_tok);
+        if (DEBUG) printf("%s\n", next_tok);
                 tokens++;
     }
-    printf("%i tokens\n", tokens);
-
-    while(tok = strtok_r(p, DELIM, &rest)) {
-        if (DEBUG) printf("i: %s\n", tok);
-        p = rest;
-    }
+    if (DEBUG) printf("%i tokens\n", tokens);
 
     free(input_copy);
-	return 0;
+    return tokens;
 }
 
-int main (int argc, const char* argv[]) {
-    char sample[] = "c1 a1 a2 a3";
-    char** result = NULL;
 
-    result = tokenize(sample, result);
+/* tokarr
+ *      returns pointer to array of strings
+ *      accepts C-string, number of tokens in string
+ */
+
+char** tokarr (char input[], int toks) {
+
+    if (strlen(input) < 2 || toks < 2)
+        return NULL;
+
+    char* buf = strdup(input);
+    int i = 0;
+    char *p;
+    char** list = (char**) malloc(sizeof(char*) * 3);
+
+    p = strtok (buf," ");  
+    while (p != NULL) {
+        list[i++] = strdup(p);
+        p = strtok (NULL, " ");
+    }
+
+    free(buf);
+    return list;
+}
+
+int main (int argc, char* argv[]) {
+    char string[] = "th thi thing";
+    char** arr = tokarr(string, count_tokens(string));
+
+
+    for (int i = 0; i < count_tokens(string); ++i) 
+        printf("%s\n", arr[i]);
+
 }
