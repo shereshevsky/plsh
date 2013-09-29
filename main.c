c24fd710694e1ddfaffa250364345ebde4fcc441
Tomas Celaya <tjcelaya@gmail.com>
2013-09-29 12:24:03 -0400

    execv working and parent waiting. it launched vim!

--- main.c
+++ main.c
@@ -13,9 +13,12 @@
 
 int main(int argc, const char * argv[]) {
 
-	char*  inputLine = calloc(INPUT_SIZE, sizeof(char));
+	char* input_line = calloc(INPUT_SIZE, sizeof(char));
+    char* found_exec;
     char** args;
     int toks;
+    pid_t childpid;
+    int childstat;
 
     // try_exec
     
@@ -23,21 +26,24 @@ int main(int argc, const char * argv[]) {
 
         prompt();
 
-		sfgetstdin(inputLine, INPUT_SIZE);
+		sfgetstdin(input_line, INPUT_SIZE);
 
-        toks = count_tokens(inputLine);
-        args = tokarr(inputLine, toks);
+        toks = count_tokens(input_line);
+        args = tokarr(input_line, toks);
 
         if (args == NULL) {
             args = (char**) malloc(sizeof(char*));
-            args[0] = inputLine;
+            args[0] = input_line;
 
         }
-        // for (int i = 0; i < toks; ++i) 
-        //     printf(":%s\n", args[i]);
+
+        envvar(toks, args);
+
+        for (int i = 0; i < toks; ++i) 
+            printf(":%s\n", args[i]);
 
 		if (DEBUG) {
-            printf("you typed: |%s|\n", inputLine);
+            printf("you typed: |%s|\n", input_line);
 		}
 
         if (strcmp(args[0], "cd") == 0) {
@@ -45,17 +51,51 @@ int main(int argc, const char * argv[]) {
             continue;
 
         } else if (strcmp(args[0], "viewproc") == 0) {
+            if (toks != 2) {
+                printf("Please provide a (singular) proc to view\n");
+                continue;
+            }
             viewproc_builtin(args[1]);
             continue;
 
         } else if (strcmp(args[0], "exit") == 0) {
-            printf("Exit\n\n");
-			return -1;
-		}
-		inputLine[0] =  '\0';
+            printf("Exit\n");
+			return 0;
+
+		} else {
+            found_exec = try_exec(args[0]);
+            if (found_exec == NULL) {
+                printf("Command not found in path\n");
+                continue;
+            }
+
+            args[0] = found_exec;
+            if (DEBUG) printf("EXEC:%s\n", found_exec);
+
+
+            args[toks] = '\0'; 
+            printf("\n\n%i\n\n", toks);
+
+            for(int i = 0; i < toks + 1; i++)
+                printf("EXEC: %i : %s\n", i, args[i]);
+
+            // execv(found_exec, args);
+
+                //splitting args for piping should happen here
+
+            if((childpid = fork()) == 0) {
+                //in child proc
+                execv(found_exec, args);
+            } else {
+                childstat = wait(&childstat);
+            }
+
+
+        }
+		input_line[0] =  '\0';
 	} while (1);
 
     //never make it here?
-	// free(inputLine);
+	// free(input_line);
 	return 0;
 }
